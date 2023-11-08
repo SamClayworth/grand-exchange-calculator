1158d8bd7abb6bea134efecd397d564452f0f9af
SamClayworth <clayworth6@hotmail.com>
2023-11-08 17:40:31 -0500

    Added Clear() functionality

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ import { items } from "./prices.js";
 let totalPrice = 0;
 
 const totals = document.querySelector(".total");
+const totalAmount = document.getElementById("total-amount");
 
 const itemCalculator = document.querySelector(".items");
 
-const itemPrices = [];
+let itemPrices = [];
 
 items.forEach((item) => {
   // creates a new table element for every item
   const itemTable = document.createElement("table");
+
   //create a table row for every item
   const itemRow = document.createElement("tr");
   // create table cells for item name, price, quantity, and total
@@ -21,9 +23,10 @@ items.forEach((item) => {
   itemPriceCell.textContent = `$${item.price}`;
 
   const quantityInputCell = document.createElement("td");
-  const quantityInput = document.createElement("input");
+  let quantityInput = document.createElement("input");
   quantityInput.type = "number";
   quantityInput.min = "0";
+  quantityInput.value = "0";
 
   const itemPriceResultCell = document.createElement("td");
   itemPriceResultCell.textContent = "$0";
@@ -48,11 +51,16 @@ items.forEach((item) => {
 
 const calculateTotalPrice = () => {
   totalPrice = itemPrices.reduce((total, price) => total + (price || 0), 0);
-  totals.innerHTML = `
-<h1 class="total-title">Total Purchase</h1>
-<p>$${totalPrice}</p>
-`;
   console.log(totalPrice);
+  totalAmount.textContent = `$${totalPrice}`;
 };
+
+document.getElementById("clear-btn").addEventListener("click", () => {
+  totalAmount.textContent = "$0";
+  const quantityInputs = document.querySelectorAll("input[type='number']");
+  quantityInputs.forEach((input) => {
+    input.value = 0;
+  });
+});
 
 calculateTotalPrice();
